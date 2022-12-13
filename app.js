e47785fff9e1d2f9877ae8835c30b2773b183555
Bin <k1234988@gmail.com>
2022-12-13 15:20:53 +0800

    codeisUpload

---
+++ app.js
@@ -0,0 +1,33 @@
+const showdown = require('showdown');
+const express = require('express')
+const fs=require('fs');
+const app = express();
+const converter = new showdown.Converter();
+//利用fs讀檔
+fs.readFile('./demo.md','utf-8',function(err,data){
+    if(err){
+        console.log("出錯")
+        return
+    } //偵錯
+    html = converter.makeHtml(data);//md轉乘html格式 
+    //利用fs寫入index.html
+    fs.writeFile("./index.html",html,function(err,data){
+        if(err){
+            console.log(err);
+        return;
+        }//寫入的偵錯
+    })
+})
+app.get("/",(req,res)=>{
+    fs.readFile("./index.html",function(err,data){
+        if(err){
+        console.log("讀檔出錯");
+        }
+        res.send(data.toString());
+        res.end()
+    })
+
+})
+app.listen(8000,()=>{
+    console.log("Server is running on 8000")
+})
